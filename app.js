6851f32714349b338d6535ce235f9d17219b7e7c
Yash Dongre <83724357+yashdongre146@users.noreply.github.com>
2023-12-08 12:46:12 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("Using Express for the first time.");
-    next();
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use("/add-product", (req, res, next) => {
+    res.send("<form action='/showOutput' method='POST'><input type='text' name='title'><input type='text' name='size'><button type='submit'>Add Product</button></form>")
 })
-app.use((req, res, next) => {
+app.post("/showOutput", (req, res, next) => {
+    console.log(`Title is ${req.body.title} and size is ${req.body.size}`);
+    res.redirect('/')
+})
+app.use("/", (req, res, next) => {
     res.send("<h1>Hello World!</h1>")
 })
 
-app.listen(3000);
+app.listen(4000);
 
