ed5bf3c10cf80f08d0bcc5fda2b0eadb23a29fff
Yash Dongre <83724357+yashdongre146@users.noreply.github.com>
2023-12-08 18:01:36 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use("/add-product", (req, res, next) => {
-    res.send("<form action='/showOutput' method='POST'><input type='text' name='title'><input type='text' name='size'><button type='submit'>Add Product</button></form>")
-})
-app.post("/showOutput", (req, res, next) => {
-    console.log(`Title is ${req.body.title} and size is ${req.body.size}`);
-    res.redirect('/')
-})
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
 app.use("/", (req, res, next) => {
-    res.send("<h1>Hello World!</h1>")
+    res.status(404).send("<h1>Page not found</h1>")
 })
 
 app.listen(4000);
