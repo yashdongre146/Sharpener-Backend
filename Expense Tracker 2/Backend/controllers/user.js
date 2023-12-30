3d339035d9cd00ede5721c939e0513b42b9e7327
Yash Dongre <83724357+yashdongre146@users.noreply.github.com>
2023-12-30 12:23:29 +0530

    Encryption added

--- Expense Tracker 2/Backend/controllers/user.js
+++ Expense Tracker 2/Backend/controllers/user.js
@@ -1,9 +1,13 @@
 const User = require('../models/user')
+const bcryptjs = require('bcryptjs')
 
-exports.addUser = async (req, res) => {
+exports.addUser =  (req, res) => {
    try {
-        const user = await User.create(req.body);
-        res.json(user);
+        const {name, email, password} = req.body;
+        bcryptjs.hash(password, 10, async (err, hash)=>{
+            const user = await User.create({name, email, password: hash});
+            res.json(user);
+        })
     } catch (err) {
             res.status(400).json();
     }
@@ -11,15 +15,17 @@ exports.addUser = async (req, res) => {
 exports.checkUser = (req, res) => {
     User.findAll({where: {email : req.params.emailId}}).then((user)=>{
         if (user.length > 0) {
-            if(user[0].dataValues.password === req.params.password){
-                res.json(user);
-            }
-            else{
-                res.status(401).json();
-            }
+            bcryptjs.compare(req.params.password, user[0].dataValues.password, (err, resp)=>{
+                if (!err) {
+                    res.json(user);
+                } else {
+                    res.status(401).json();
+                }
+            })
         } else {
             res.status(401).json()
         }
 
     })
+
 }
